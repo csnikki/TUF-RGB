549e163289d078efd954b38499bae0f8100b159d
nromeo02 <nicol.romo706@gmail.com>
2022-06-28 17:37:20 +0200

    C++ support.

--- TUFRGB.h
+++ TUFRGB.h
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 
+#define TUF_API
+
 #define TUF_SUCCESS 0
 #define TUF_ERROR 1
 
@@ -24,15 +26,24 @@ typedef enum
 typedef void* TUFHandle;
 typedef uint8_t TUFByte;
 
-TUFHandle TUFRGB_Open();
-void TUFRGB_Close(TUFHandle handle);
+#if defined(__cplusplus)
+extern "C"
+{
+#endif
 
-int TUFRGB_Set(
+TUF_API TUFHandle TUFRGB_Open();
+TUF_API void TUFRGB_Close(TUFHandle handle);
+
+TUF_API int TUFRGB_Set(
     TUFHandle handle,
     const TUFAction action,
     const TUFSpeed speed,
     const TUFByte red,
     const TUFByte green,
     const TUFByte blue);
+
+#if defined(__cplusplus)
+}
+#endif
 
 #endif /* _TUFRGB_H */
